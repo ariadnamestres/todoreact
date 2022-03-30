4e16b78ca151a9fa5725a72d5ef6f5e5fc8d034b
Ariadna Mestres <amestresjimenez@gmail.com>
2022-03-30 19:57:30 +0200

    as

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./styles.css";
+const EndPoint = "https://tc-todo-2022.herokuapp.com/todos";
+function AfegirTodo({ onTodoAdded }) {
+  const titleRef = useRef();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = titleRef.current.value;
+        titleRef.current.value = "";
+        fetch(EndPoint, {
+          method: "POST",
+          body: JSON.stringify({ title }),
+        })
+          .then((response) => response.json())
+          .then((json) => onTodoAdded(json));
+      }}
+    >
+      <input ref={titleRef} />
+      <input type="submit" value="Afegir" />
+    </form>
   );
 }
 
-export default App;
+function TodoItem({ todo, onUpdated }) {
+  return (
+    <li
+      className={todo.completed ? "completed" : "pending"}
+      onClick={() => {
+        fetch(`${EndPoint}/${todo.id}`, {
+          method: "POST",
+          body: JSON.stringify({ completed: !todo.completed }),
+        })
+          .then((response) => response.json())
+          .then((json) => onUpdated(json));
+      }}
+    >
+      {todo.title}
+    </li>
+  );
+}
+
+export function getTodos() {
+  return fetch(EndPoint).then((response) => response.json());
+}
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    getTodos().then(setTodos);
+
+    const intervalID = setInterval(() => {
+      getTodos().then(setTodos);
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1> LListat de TODOS </h1>
+      <button onClick={() => getTodos().then(setTodos)}>Refresh</button>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onUpdated={(updatedTodo) =>
+              setTodos(
+                todos.map((currentTodo) =>
+                  currentTodo.id === updatedTodo.id ? updatedTodo : currentTodo
+                )
+              )
+            }
+          />
+        ))}
+      </ul>
+      <AfegirTodo onTodoAdded={(todo) => setTodos([...todos, todo])} />
+      {/* <pre> {JSON.stringify(todos, null, 2)} </pre> */}
+    </div>
+  );
+}
